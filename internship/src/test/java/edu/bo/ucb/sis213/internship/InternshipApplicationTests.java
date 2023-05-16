e9da47923bed9a25e80bf3870587cc00d3040a41
Edward-Gomez-Q <osval_edward_13@hotmail.com>
2023-05-16 16:36:48 -0400

    Creando proyectos vacios

---
+++ internship/src/test/java/edu/bo/ucb/sis213/internship/InternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bo.ucb.sis213.internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
