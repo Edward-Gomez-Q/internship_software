94c374e23d9fe6b45793fc754aa86adf682e1fb4
gabyportu <gabriela.portugal@ucb.edu.bo>
2023-06-09 23:33:17 -0400

    intership services

---
+++ intership_frontend/lib/services/intership_services.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:http/http.dart' as http;
+import '../models/intership_model.dart';
+import 'globals.dart';
+
+class IntershipServices {
+  static Future<String> addIntership(IntershipModel intership) async {
+    print("entro al servicio");
+    Map data = {
+      'titleIntership': intership.titleIntership,
+      'department': intership.department,
+      'deadline': intership.deadline,
+      'days': intership.days,
+      'durations': intership.durations,
+      'startTime': intership.startTime,
+      'endTime': intership.endTime,
+      'description': intership.description,
+      'urlPDF': intership.urlPDF,
+      'urlWord': intership.urlWord,
+      'requirements': intership.requirements,
+      'careers': intership.careers,
+      'knowledge': intership.knowledge,
+    };
+    var body = json.encode(data);
+    var url = Uri.parse('$baseUrl/company/$intership.companyId}/intership');
+    http.Response response = await http.post(
+      url,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: body,
+    );
+    print('Response Agregar pasantia: ${response.body}');
+
+    if (response.statusCode == 200) {
+      Map responseMap = json.decode(response.body);
+      if (responseMap["code"] == "200") {
+        return 'Intership added';
+      } else {
+        return 'Error 404';
+      }
+    } else {
+      return 'Error 404';
+    }
+  }
+}
