6d0569a830aa19d80eb9ecad1aaee193be710a8f
gabyportu <gabriela.portugal@ucb.edu.bo>
2023-06-11 15:05:43 -0400

    arreglos frontend

--- intership_frontend/lib/screens/student/register_student/register_form.dart
+++ intership_frontend/lib/screens/student/register_student/register_form.dart
@@ -190,7 +190,9 @@ class FormRegister extends StatelessWidget {
                       child: RadioListTile(
                         title: Text("Estudiante"),
                         value: 1,
-                        groupValue: 1,
+                        groupValue: BlocProvider.of<StudentCubit>(context)
+                            .state
+                            .validacionUCB,
                         onChanged: (value) {
                           BlocProvider.of<StudentCubit>(context)
                               .updateValidacionUCB(value ?? 1);
@@ -202,10 +204,12 @@ class FormRegister extends StatelessWidget {
                       child: RadioListTile(
                         title: Text("Graduado"),
                         value: 2,
-                        groupValue: 1,
+                        groupValue: BlocProvider.of<StudentCubit>(context)
+                            .state
+                            .validacionUCB,
                         onChanged: (value) {
                           BlocProvider.of<StudentCubit>(context)
-                              .updateValidacionUCB(value ?? 1);
+                              .updateValidacionUCB(value ?? 2);
                         },
                       ),
                     ),
